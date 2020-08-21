7c39b85703b8592e0b1b82faf10538ca009acc54
shukal94 <sergey.shukalovich@gmail.com>
2020-08-21 19:55:53 +0300

    upd version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'zebrunnerpy',
   packages = ['zebrunnerpy'],
-  version = '0.1.5',
+  version = '0.1.6',
   license='MIT',
   description = 'Python3 connector for Zebrunner-reporting',
   author = 'Sergey Shukalovich',
