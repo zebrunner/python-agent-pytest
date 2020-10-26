6c823cc737fbac5e45d2e2db882a0ff57a81f19c
Anatoliy Platonov <p4m.dev@gmail.com>
2020-10-26 14:45:55 +0300

    WIP: Selenium injecting prototype

--- zebrunnerpy/__init__.py
+++ zebrunnerpy/__init__.py
@@ -1,6 +1,27 @@
-__all__ = ['zafira_state', 'client', 'resource_constants', 'resources', 'plugin', 'listener',
-           'connector_obj', 'PyTestZafiraListener', 'handler', 'ZebrunnerRestHandler']
+__all__ = [
+    'zafira_state', 'client', 'resource_constants', 'resources', 'plugin', 'listener',
+    'connector_obj', 'PyTestZafiraListener', 'handler', 'ZebrunnerRestHandler'
+]
+
+import logging
+from pprint import pprint
 
 from .plugin import connector_obj
 from .listener import PyTestZafiraListener
 from .handler import ZebrunnerRestHandler
+
+
+logger = logging.getLogger('ui')
+
+
+try:
+    from selenium import webdriver
+
+    class CustomDriver(webdriver.Remote):
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+            pprint(self.__dict__)
+
+    webdriver.WebDriver = CustomDriver
+except ImportError:
+    logger.warning('Selenium library is not installed.')
