8050084cb03cc8fdda4ff0566b0ef0abab64fe8e
Anatoliy Platonov <p4m.dev@gmail.com>
2020-11-24 14:40:23 +0300

    Add examples folder

---
+++ examples/pytest_example/test_stuf.py
@@ -0,0 +1,15 @@
+import pytest
+
+
+def test_success() -> None:
+    assert True
+
+
+@pytest.mark.maintainer("admin")
+def test_maintainer() -> None:
+    assert True
+
+
+@pytest.mark.skip
+def test_skip() -> None:
+    assert True
