d1bd4017fc5355c18da1da92272b689386d9d790
Anatoliy Platonov <p4m.dev@gmail.com>
2021-05-12 14:31:28 +0300

    Change version

--- src/pytest_zebrunner/__init__.py
+++ src/pytest_zebrunner/__init__.py
@@ -8,7 +8,7 @@ from pytest_zebrunner.attachments import (
     attach_test_screenshot,
 )
 
-__version__ = "1.0.0.dev4"
+__version__ = "1.0.0.post1"
 __all__ = [
     "attach_test_artifact_reference",
     "attach_test_run_artifact_reference",
